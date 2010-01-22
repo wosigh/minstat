016a8bce8b4f672f235ee5833d3558b98b91b83c
Tobias B. Hlavka <TBH@ceres-NB001.local>
2010-01-22 19:12:00 +0100

    Initial commit

---
+++ app/assistants/main-assistant.js
@@ -0,0 +1,232 @@
+function MainAssistant(argFromPusher) {
+// TODO
+// browser-like refresh button
+// refresh on active
+}
+
+MainAssistant.prototype = {
+
+	setup: function() {
+		Ares.setupSceneAssistant(this);
+		this.btnRefreshTap();
+	},
+
+	cleanup: function() {
+		Ares.cleanupSceneAssistant(this);
+	},
+
+	saveSettings: function(event, inSender) {
+		myCookie = new Mojo.Model.Cookie('settings');
+		var myObject = {"clockrateFirst": this.modSettings.clockrateFirst,
+										"clockrateFurther": this.modSettings.clockrateFurther,
+			 							"freeMinutes": this.modSettings.freeMinutes,
+										"freeSMS": this.modSettings.freeSMS,
+										"payrollDay": this.modSettings.payrollDay};
+		myCookie.put(myObject);
+	},
+
+	loadSettings: function(event, inSender) {
+		myCookie = new Mojo.Model.Cookie('settings');
+		var myObject = myCookie.get();
+		if (myObject !== undefined) {
+			this.modSettings.clockrateFirst = myObject.clockrateFirst;
+			this.modSettings.clockrateFurther = myObject.clockrateFurther;
+			this.modSettings.freeMinutes = myObject.freeMinutes;
+			this.modSettings.freeSMS = myObject.freeSMS;
+			this.modSettings.payrollDay = myObject.payrollDay;
+		} else {
+			this.modSettings.clockrateFirst = 60;
+			this.modSettings.clockrateFurther = 60;
+			this.modSettings.freeMinutes = 100;
+			this.modSettings.freeSMS = 100;
+			this.modSettings.payrollDay = 1;
+		}
+			this.controller.modelChanged(this.modSettings,this);
+	},
+
+	btnRefreshTap: function(event, inSender) {
+
+//TODO: read from service and from appinfo.json
+		this.getServiceVersion();
+		this.loadSettings();
+		this.loadFromService();
+
+	},
+	intPickSMSChange: function(event, inSender) {
+		this.saveSettings();
+		this.loadFromService();
+	},
+	intPickMinChange: function(event, inSender) {
+		this.saveSettings();
+		this.loadFromService();
+	},
+	datePickChange: function(event, inSender) {
+		this.saveSettings();
+	},
+	intPickClockrateFirstChange: function(event, inSender) {
+		this.saveSettings();
+	},
+	intPickClockrateFurtherChange: function(event, inSender) {
+		this.saveSettings();
+	},
+
+	loadFromService: function(event, inSender) {
+		this.controller.serviceRequest('palm://de.hlavka.minstatservice', {
+        method: 'getSQLData',
+        parameters: {subscribe:false, "cashday":this.modSettings.payrollDay,"clockrateFirst":this.modSettings.clockrateFirst,"clockrateFurther":this.modSettings.clockrateFurther},
+        onSuccess: function(response) {
+							var cntSMS = response.SMS;
+							var cntMins = response.minutes;
+
+							cntMins=this.roundNumber(cntMins,2);
+
+							this.modProgrMins.progrMinutes = (cntMins/this.modSettings.freeMinutes);
+							this.modProgrSMS.progrSMS = (cntSMS/this.modSettings.freeSMS);
+							this.controller.modelChanged(this.modProgrSMS,this);
+							this.controller.modelChanged(this.modProgrMins,this);
+
+							if (this.modSettings.freeMinutes === 0) {
+								this.controller.get("lblMinutes").update(cntMins);
+							} else {
+								this.controller.get("lblMinutes").update(cntMins+"/"+this.modSettings.freeMinutes);
+
+
+								if (this.modProgrMins.progrMinutes > 1) {
+									this.controller.get("modMinSmiley").update(":-(");
+								} else if (this.modProgrMins.progrMinutes <= this.getDaysSinceLastPayroll()/this.days_between(this.getDateOfLastPayroll(),this.getDateOfNextPayroll())) {
+									this.controller.get("modMinSmiley").update(":-)");
+								} else if (this.modProgrMins.progrMinutes > this.getDaysSinceLastPayroll()/this.days_between(this.getDateOfLastPayroll(),this.getDateOfNextPayroll())) {
+									this.controller.get("modMinSmiley").update(":-|");
+								}
+
+							}
+							
+							if (this.modSettings.freeSMS === 0) {
+								this.controller.get("lblSMS").update(cntSMS);
+							} else {
+								this.controller.get("lblSMS").update(cntSMS+"/"+this.modSettings.freeSMS);
+
+								if (this.modProgrSMS.progrSMS > 1) {
+									this.controller.get("lblSMSSmile").update(":-(");
+								} else if (this.modProgrSMS.progrSMS <= this.getDaysSinceLastPayroll()/this.days_between(this.getDateOfLastPayroll(),this.getDateOfNextPayroll())) {
+									this.controller.get("lblSMSSmile").update(":-)");
+								} else if (this.modProgrSMS.progrSMS > this.getDaysSinceLastPayroll()/this.days_between(this.getDateOfLastPayroll(),this.getDateOfNextPayroll())) {
+									this.controller.get("lblSMSSmile").update(":-|");
+								}
+
+							}
+							this.controller.get("lblStatus").update("");
+
+			}.bind(this), onFailure: function(response) {
+							this.controller.get("lblStatus").update("Service not Running! " + response);
+			}.bind(this)
+    });
+	},
+
+	intPickPayrollChange: function(event, inSender) {
+		this.saveSettings();
+		this.loadFromService();
+	},
+
+	days_between: function(date1, date2) {
+
+    // The number of milliseconds in one day
+    var ONE_DAY = 1000 * 60 * 60 * 24;
+
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+
+    // Calculate the difference in milliseconds
+    var difference_ms = Math.abs(date1_ms - date2_ms);
+    
+    // Convert back to days and return
+    return Math.round(difference_ms/ONE_DAY);
+
+	},
+
+	getDaysSinceLastPayroll: function() {
+
+		var now = new Date(); // today
+
+		var month = now.getMonth();
+		var year = now.getFullYear();
+
+  	if (now.getDate() < this.modSettings.payrollDay) {
+			month = month-1;
+			
+			if (month === 0) {
+				month = 12;
+				year = year-1;
+			}
+		}
+		var lastPayroll = new Date(year, month, this.modSettings.payrollDay);
+		return this.days_between(now,lastPayroll);
+	},
+
+	getDateOfLastPayroll: function() {
+
+		var now = new Date(); // today
+
+		var month = now.getMonth();
+		var year = now.getFullYear();
+
+  	if (now.getDate() < this.modSettings.payrollDay) {
+			month = month-1;
+			
+			if (month === 0) {
+				month = 12;
+				year = year-1;
+			}
+		}
+		var lastPayroll = new Date(year, month, this.modSettings.payrollDay);
+		return lastPayroll;
+	},
+
+	getDateOfNextPayroll: function() {
+
+		var now2 = new Date(); // today
+
+		var month = now2.getMonth();
+		var year = now2.getFullYear();
+
+  	if (now2.getDate() >= this.modSettings.payrollDay) {
+			month = month+1;
+			if (month == 13) {
+				month = 1;
+				year = year+1;
+			}
+		}
+		var nextPayroll = new Date(year, month, this.modSettings.payrollDay);
+		return nextPayroll;
+	},
+
+	roundNumber: function(num, dec) {
+		var result = Math.round(num*Math.pow(10,dec))/Math.pow(10,dec);
+		return result;
+	},
+
+	getServiceVersion: function(event, inSender) {
+
+		this.controller.serviceRequest('palm://de.hlavka.minstatservice', {
+        method: 'getVersion',
+        parameters: {subscribe:false, "":""},
+        onSuccess: function(response) {
+					this.controller.get("lblVersion").update("MinStat V1.0 - Service V"+response.version.toString());
+        }.bind(this), onFailure: function(response) {
+							this.controller.get("lblStatus").update("Service not Running! " + response);
+			}.bind(this)
+    });
+
+	},
+
+	integerPicker1Change: function(event, inSender) {
+		this.saveSettings();
+		this.loadFromService();
+	},
+	integerPicker2Change: function(event, inSender) {
+		this.saveSettings();
+		this.loadFromService();
+	}
+
+};
---
+++ app/views/main/main-chrome.js
@@ -0,0 +1,361 @@
+opus.Gizmo({
+	name: "main",
+	dropTarget: true,
+	type: "Palm.Mojo.Panel",
+	h: "100%",
+	styles: {
+		zIndex: 2,
+		clip: true
+	},
+	chrome: [
+		{
+			name: "scroller1",
+			ontap: "",
+			type: "Palm.Mojo.Scroller",
+			l: 0,
+			t: "50",
+			h: "100%",
+			styles: {
+				cursor: "move",
+				overflow: "hidden"
+			},
+			controls: [
+				{
+					name: "header1",
+					label: "MinStat - Minutes & SMS Counter",
+					type: "Palm.Mojo.Header",
+					l: 0,
+					t: "0"
+				},
+				{
+					name: "group1",
+					dropTarget: true,
+					label: "Minutes used",
+					type: "Palm.Mojo.Group",
+					l: 0,
+					t: "55",
+					h: 100,
+					controls: [
+						{
+							name: "panel1",
+							layoutKind: "hbox",
+							dropTarget: true,
+							type: "Palm.Mojo.Panel",
+							l: 0,
+							t: 0,
+							h: 60,
+							controls: [
+								{
+									name: "picture1",
+									src: "icon_call.png",
+									autoSize: false,
+									type: "Palm.Picture",
+									l: 69,
+									w: "70",
+									t: 0,
+									h: 51
+								},
+								{
+									name: "lblMinutes",
+									ontap: "",
+									modelName: "modStatus",
+									label: "0/0",
+									labelPropertyName: "lblSMS",
+									type: "Palm.Mojo.Label",
+									l: 114,
+									r: 135,
+									w: "80%",
+									t: "0",
+									b: 16,
+									h: 29,
+									vAlign: "center",
+									styles: {
+										textAlign: "center",
+										oneLine: true
+									}
+								},
+								{
+									name: "progrMinutes",
+									modelName: "modProgrMins",
+									progress: "0.1",
+									progressPropertyName: "progrMinutes",
+									type: "Palm.Mojo.ProgressBar",
+									l: 259,
+									t: 0,
+									b: 26,
+									hAlign: "center",
+									vAlign: "center"
+								},
+								{
+									name: "modMinSmiley",
+									modelName: "modMinSmiley",
+									label: "",
+									type: "Palm.Mojo.Label",
+									l: 270,
+									w: "25%",
+									t: 0,
+									h: 29,
+									hAlign: "center",
+									vAlign: "center"
+								},
+								{
+									name: "statusMinutes",
+									showing: false,
+									type: "Palm.Mojo.StatusIndicator",
+									l: 266,
+									w: "30%",
+									t: 0,
+									vAlign: "center"
+								}
+							]
+						},
+						{
+							name: "row1",
+							dropTarget: true,
+							rowType: "last",
+							type: "Palm.Mojo.Row",
+							l: 0,
+							t: 60,
+							h: "auto"
+						}
+					]
+				},
+				{
+					name: "group2",
+					dropTarget: true,
+					label: "SMS (parts) sent",
+					type: "Palm.Mojo.Group",
+					l: 0,
+					t: 94,
+					controls: [
+						{
+							name: "panel2",
+							layoutKind: "hbox",
+							dropTarget: true,
+							type: "Palm.Mojo.Panel",
+							l: 0,
+							t: 0,
+							h: 60,
+							controls: [
+								{
+									name: "picture3",
+									src: "icon_sms.png",
+									autoSize: false,
+									type: "Palm.Picture",
+									l: -1,
+									w: 70,
+									t: 0,
+									h: 57
+								},
+								{
+									name: "lblSMS",
+									modelName: "modStatus",
+									label: "0/0",
+									labelPropertyName: "lblMinutes",
+									type: "Palm.Mojo.Label",
+									l: 60,
+									w: "80%",
+									t: 0,
+									h: "60",
+									styles: {
+										textAlign: "center",
+										oneLine: true
+									}
+								},
+								{
+									name: "progrSMS",
+									ontap: "progressBar2Tap",
+									modelName: "modProgrSMS",
+									progress: "0.1",
+									progressPropertyName: "progrSMS",
+									type: "Palm.Mojo.ProgressBar",
+									l: 167,
+									t: 0,
+									vAlign: "center",
+									styles: {
+										textColor: "",
+										bgColor: "",
+										borderColor: "",
+										bgImage: ""
+									}
+								},
+								{
+									name: "lblSMSSmile",
+									modelName: "modSMSSmiley",
+									label: "",
+									type: "Palm.Mojo.Label",
+									l: 301,
+									w: "25%",
+									t: 0,
+									h: 29,
+									hAlign: "center",
+									vAlign: "center"
+								},
+								{
+									name: "statusSMS",
+									showing: false,
+									type: "Palm.Mojo.StatusIndicator",
+									l: 302,
+									w: "30%",
+									t: 0,
+									vAlign: "center",
+									styles: {
+										bgColor: "",
+										textColor: ""
+									}
+								}
+							]
+						}
+					]
+				},
+				{
+					name: "btnRefresh",
+					ontap: "btnRefreshTap",
+					label: "refresh",
+					type: "Palm.Mojo.Button",
+					l: 0,
+					t: 239
+				},
+				{
+					name: "lblStatus",
+					label: "",
+					type: "Palm.Mojo.Label",
+					l: 0,
+					t: 299,
+					h: 48
+				},
+				{
+					name: "divider1",
+					label: "Settings",
+					type: "Palm.Mojo.Divider",
+					l: 0,
+					t: 299
+				},
+				{
+					name: "intPickPayroll",
+					ontap: "intPickPayrollTap",
+					modelName: "modSettings",
+					value: 9,
+					valuePropertyName: "payrollDay",
+					label: "Billing day",
+					min: "1",
+					max: "31",
+					onchange: "intPickPayrollChange",
+					type: "Palm.Mojo.IntegerPicker",
+					l: 0,
+					t: 381,
+					styles: {
+						textAlign: "right"
+					}
+				},
+				{
+					name: "intPickSMS",
+					ontap: "",
+					modelName: "modSettings",
+					value: 100,
+					valuePropertyName: "freeSMS",
+					label: "Monthly included SMS",
+					max: "1000",
+					onchange: "intPickSMSChange",
+					type: "Palm.Mojo.IntegerPicker",
+					l: 0,
+					t: 441,
+					styles: {
+						textAlign: "right"
+					}
+				},
+				{
+					name: "intPickMin",
+					ontap: "",
+					modelName: "modSettings",
+					value: 100,
+					valuePropertyName: "freeMinutes",
+					label: "Monthly included Mins",
+					max: "1000",
+					onchange: "intPickMinChange",
+					type: "Palm.Mojo.IntegerPicker",
+					l: 0,
+					t: 501,
+					styles: {
+						textAlign: "right"
+					}
+				},
+				{
+					name: "panel3",
+					layoutKind: "hbox",
+					dropTarget: true,
+					type: "Palm.Mojo.Panel",
+					l: 0,
+					t: 566,
+					h: 60,
+					controls: [
+						{
+							name: "integerPicker1",
+							ontap: "integerPicker1Tap",
+							modelName: "modSettings",
+							value: 60,
+							valuePropertyName: "clockrateFirst",
+							label: "Clockrate",
+							min: "1",
+							max: "60",
+							onchange: "integerPicker1Change",
+							type: "Palm.Mojo.IntegerPicker",
+							l: 0,
+							t: 0,
+							styles: {
+								textAlign: "right"
+							}
+						},
+						{
+							name: "integerPicker2",
+							modelName: "modSettings",
+							value: 60,
+							valuePropertyName: "clockrateFurther",
+							label: "/",
+							min: "1",
+							max: "60",
+							onchange: "integerPicker2Change",
+							type: "Palm.Mojo.IntegerPicker",
+							l: 220,
+							w: 100,
+							t: 0,
+							styles: {
+								textAlign: "right"
+							}
+						}
+					]
+				},
+				{
+					name: "lblVersion",
+					label: "MinStat 0 - MinstatService 0",
+					type: "Palm.Mojo.Label",
+					l: 0,
+					t: 561,
+					h: 24,
+					hAlign: "center",
+					vAlign: "center",
+					styles: {
+						textAlign: "center",
+						fontSize: "14px"
+					}
+				},
+				{
+					name: "divider4",
+					label: "Credits",
+					type: "Palm.Mojo.Divider",
+					l: 0,
+					t: 562
+				},
+				{
+					name: "html1",
+					content: "<font size=\"2\">\n  If you enjoy this application and find it useful, please consider a small donation to the developer for his efforts - just think about how much money you save by having this app ;-)  \n</font><br><br>\n  <form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\"><input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\n<input type=\"hidden\" name=\"hosted_button_id\" value=\"11298589\">\n<input type=\"image\" src=\"https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\">\n<img alt=\"\" border=\"0\" src=\"https://www.paypal.com/de_DE/i/scr/pixel.gif\" width=\"1\" height=\"1\">\n</form>\n<hr>\n<font size=\"2\">\n&copy; 2010 by Tobias Hlavka<br>\n  \"Milky\"-Icons by <a href=\"http://min.frexy.com/article/milky_a_free_\n  vector_icon_set_part_1/\">MinTran</a><br>\n  Visit Precentral for further information.<br><br>\n  The above values are for information only and without responsibility - they can be really really wrong if you e.g. did fully erase your device less than one month ago.\n</font>",
+					type: "Palm.Mojo.Html",
+					l: 9,
+					w: 305,
+					t: 595,
+					h: 275
+				}
+			]
+		}
+	]
+});
